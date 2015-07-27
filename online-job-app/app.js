7939502c033c9a499fe6b58e94fa75d557336a56
Fully34 <cfullinw34@gmail.com>
2015-07-27 17:35:58 -0600

    working on part iv

--- online-job-app/app.js
+++ online-job-app/app.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var applicant = require('./models/database.js')
 
 var app = express();
 app.set('view engine', 'jade');
@@ -18,11 +20,57 @@ app.get('/applicants', function(req, res){
 
 // creates and applicant
 app.post('/applicant', function(req, res){
+
+
+    var name = req.body.name;
+    var bio = req.body.bio;
+    var skills = req.body.skills;
+    var years = req.body.years;
+    var why = req.body.why;
+
+    var applier = {
+        name : name,
+        bio : bio, 
+        skills : skills,
+        years : years,
+        why : why
+    }
+
+    // Since the body is in the exact same format as the schema, we can just pass the body in
+    var thisApplicant = new Applicant(applier);
+
+    thisApplicant.save(function(err, doc) {
+
+        if(err) {
+
+            console.log('Son Of A!')
+        } else {
+
+            res.redirect
+        }
+    })
+
+    thisApplicant.save();
 	// Here is where you need to get the data
 	// from the post body and store it in the database
-	res.send('No funciona');
+	Applicant.find({}, function(err, doc) {
+
+        if (err) {
+
+            console.log('ERROR! ' + err.name);
+
+        } else {
+
+            res.render('applicants', {applicants : doc})
+        }
+    })
 });
 
-var server = app.listen(8441, function() {
+
+//============================== server/db ==============================//
+        
+mongoose.connect('mongodb://localhost/company');
+
+var server = app.listen(9001, function() {
 	console.log('Express server listening on port ' + server.address().port);
 });
---
+++ online-job-app/models/database.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose');
+
+
+// var Applicant = function(name, bio, skills, years, why) {
+//     this.name = name;
+//     this.bio = bio;
+//     this.skills = skills;
+//     this.years = years;
+//     this.why = why;
+// }
+
+
+//============================== schema ==============================//
+
+var Applicant = mongoose.model('Applicant', {name : {type : String}, bio : {type : String}, skills : String, years : {type : String}, why : {type : String}});
+        
+
+
+module.exports = Applicant;
